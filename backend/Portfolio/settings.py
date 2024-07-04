05abc975281d57690f00456790a5c4c5e8f61237
Damon <56492050+NoCapCbas@users.noreply.github.com>
2024-07-04 14:53:38 -0400

    Update settings.py

--- backend/Portfolio/settings.py
+++ backend/Portfolio/settings.py
@@ -37,7 +37,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://localhost',
     'http://damondiaz.xyz',
     'https://damondiaz.xyz',
-    'https://backend.damondiaz.xyz',
+    'https://admin.damondiaz.xyz',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True

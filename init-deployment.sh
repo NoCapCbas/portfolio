df3866803887bfbe0c3f266b4949c39ddb62840d
Damon <56492050+NoCapCbas@users.noreply.github.com>
2024-09-09 07:24:07 -0400

    Update and rename prod.sh to init-deployment.sh

--- init-deployment.sh
+++ init-deployment.sh
@@ -3,7 +3,4 @@ export DEV_EMAIL=example@domain.com
 export HOST_DOMAIN=example.com
 
 # Run the Docker container with environment variables
-docker run -d \
-  -e DEV_EMAIL="$DEV_EMAIL" \
-  -e HOST_DOMAIN="$HOST_DOMAIN" \
-  ghcr.io/nocapcbas/portfolio:prod
+docker compose -f docker-compose.prod.yml up -d --build --force-recreate

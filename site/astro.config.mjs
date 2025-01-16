b607393a1d19ffb6288637d04f3e6e0517c5ad72
unknown <ddiazny919@gmail.com>
2025-01-15 22:22:59 -0500

    reformatted Github.astro

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,7 +7,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://damondiaz.xyz',
 
   integrations: [
       mdx(),
